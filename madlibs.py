898f22b2be3cc0adb833bdbf73c5e3008fa6995c
NandiniSHinduja <69416051+NandiniSHinduja@users.noreply.github.com>
2021-11-17 20:36:09 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+name=input("Name:")
+age=input("Age:")
+school=input("School:")
+city=input("City:")
+madlib=f"I am {name}. I am {age} years old. I study at {school}. I live in {city}"
+print(madlib)
